2968b66a0e8fb4fff0eb039cac407d2ce8b29b43
Peter Lavigne <peter.lavigne@appfolio.com>
2019-01-09 11:25:44 -0800

    pl -  display images on index page
    pl -  show newest images first

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,4 @@
-class ImagesController < ActionController::Base
-  protect_from_forgery with: :exception
-
+class ImagesController < ApplicationController
   def new
     @image = Image.new
   end
@@ -18,5 +16,7 @@ class ImagesController < ActionController::Base
     @url = Image.find(params[:id]).url
   end
 
-  def index; end
+  def index
+    @urls = Image.order('created_at DESC').map(&:url)
+  end
 end
