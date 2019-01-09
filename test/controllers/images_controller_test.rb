c9e8803ae35c571179b690dd3ab075bc7cd6f2ad
Peter Lavigne <peter.lavigne@appfolio.com>
2019-01-09 10:29:23 -0800

    pl -  update test fixtures to include a valid image
    pl -  check that there is exactly one image loaded onto the front page

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -2,12 +2,22 @@ require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
   test 'should render landing page' do
-    get root_url
+    get root_path
     assert_response :ok
   end
 
   test 'should get new' do
     get new_image_path
+    assert_response :ok
+  end
+
+  test 'should show image' do
+    get image_path(Image.first)
+    assert_response :ok
+  end
+
+  test 'should show index' do
+    get images_path
     assert_response :ok
   end
 
@@ -17,5 +27,10 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
         url: 'https://learn.appfolio.com/apm/www/images/apm-logo-v2.png'
       } }
     end
+  end
+
+  test 'should be an image on the landing page' do
+    get root_path
+    assert_select 'img', count: 1
   end
 end
