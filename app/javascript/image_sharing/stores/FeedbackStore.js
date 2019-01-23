30a8c0b7cd7db76a0f397ce83d3376fa66e3c653
Peter Lavigne <peter.lavigne@appfolio.com>
2019-01-22 16:10:46 -0800

    pl - add feedback store

--- app/javascript/image_sharing/stores/FeedbackStore.js
+++ app/javascript/image_sharing/stores/FeedbackStore.js
@@ -1,5 +1,18 @@
+import { action, observable } from 'mobx';
+
 export class FeedbackStore {
-  /* Implement your feedback store*/
+  @observable name = "";
+  @observable feedback = "";
+
+  @action
+  setName = (name) => {
+    this.name = name;
+  };
+
+  @action
+  setFeedback = (feedback) => {
+    this.feedback = feedback;
+  };
 }
 
 export default FeedbackStore;
